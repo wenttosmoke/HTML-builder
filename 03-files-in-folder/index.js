78a109c4abe4345dd03a05a26012a12577dcb452
Vlad <vlad_pumpur@mail.ru>
2022-11-05 02:29:53 +0300

    add solution to files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,34 @@
+import { readdir, stat } from 'node:fs/promises';
+import path from 'node:path';
+
+const url = './03-files-in-folder/secret-folder'
+
+function handleError() {
+   
+throw Error('Unexpected error, incorrect directory')
+}
+async function checkDir(url) {
+    let files
+    try {
+        files =  await readdir(url,{withFileTypes: true})
+    }
+    catch {
+        handleError()
+    }
+
+    for (const file of files) {
+        if (!file.isDirectory()) {
+            console.log(
+                path.basename(file['name'], path.extname(file['name'])), ' - ',
+                !path.extname(file['name'])?"none":path.extname(file['name']), ' - ',
+                (await stat(url + '/' + file['name'])).size / 1024, ' kb' )
+        }
+        else
+         {
+            checkDir(url+ `/${file['name']}`)
+         }   
+}
+}
+checkDir(url)
+
+ 
